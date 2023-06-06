08825e5d923f63e5bf37d47c34576e06fd49d5d7
TraceyGeneau <128385495+TraceyGeneau@users.noreply.github.com>
2023-06-06 13:17:04 -0400

    fixed git ignore

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "bf0e9b297f8b7fcfa682a35cb61b6a1b"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "d440884fcfd84da889edcc3452220f67"
